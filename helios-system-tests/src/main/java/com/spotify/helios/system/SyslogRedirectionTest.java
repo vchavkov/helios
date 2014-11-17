258a6a6bc66d17dcd911c53fc2fd91a6a63c03ad
Daniel Norberg <dano@spotify.com>
2014-11-17 18:23:56 +0900

    wip

--- helios-system-tests/src/main/java/com/spotify/helios/system/SyslogRedirectionTest.java
+++ helios-system-tests/src/main/java/com/spotify/helios/system/SyslogRedirectionTest.java
@@ -22,16 +22,26 @@
 package com.spotify.helios.system;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.net.HostAndPort;
 
 import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.LogStream;
+import com.spotify.docker.client.messages.ContainerConfig;
+import com.spotify.docker.client.messages.ContainerInfo;
+import com.spotify.docker.client.messages.HostConfig;
+import com.spotify.docker.client.messages.PortBinding;
+import com.spotify.helios.Polling;
+import com.spotify.helios.common.descriptors.Job;
 import com.spotify.helios.common.descriptors.JobId;
 import com.spotify.helios.common.descriptors.TaskStatus;
 
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeoutException;
 
 import static com.spotify.docker.client.DockerClient.LogsParameter.STDERR;
 import static com.spotify.docker.client.DockerClient.LogsParameter.STDOUT;
@@ -41,9 +51,57 @@ import static java.util.Arrays.asList;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.assertEquals;
 
-// TODO (dano): open source the syslog-redirector and enable this test
-@Ignore("This needs the syslog-redirector")
 public class SyslogRedirectionTest extends SystemTestBase {
+
+  public static final String REDIRECTOR = "spotify/syslog-redirector:0.0.5";
+
+  private HostAndPort sinkHostAndPort;
+  private String sink;
+
+  @Before
+  public void setup() throws Exception {
+    final DockerClient docker = getNewDockerClient();
+//    docker.pull(REDIRECTOR);
+    final ContainerConfig redirectorConfig = ContainerConfig.builder()
+        .image(REDIRECTOR)
+        .volumes("/helios")
+        .cmd("cp", "/syslog-redirector", "/helios")
+        .build();
+    final String redirector = randomName("redirector");
+    docker.createContainer(redirectorConfig, redirector);
+    docker.startContainer(redirector);
+    docker.waitContainer(redirector);
+
+    sink = randomName("sink");
+    final ContainerConfig sinkConfig = ContainerConfig.builder()
+        .image(BUSYBOX)
+        .cmd("nc", "-p", "4711", "-l")
+        .build();
+    final int syslog = temporaryPorts.localPort("syslog");
+    final HostConfig sinkHostConfig = HostConfig.builder()
+        .portBindings(ImmutableMap.of(syslog + "/tcp", asList(PortBinding.of("0.0.0.0", 4711))))
+        .build();
+    docker.createContainer(sinkConfig, sink);
+    docker.startContainer(sink, sinkHostConfig);
+    awaitContainerRunning(docker, sink);
+
+    sinkHostAndPort = HostAndPort.fromParts(DOCKER_HOST.address(), 4711);
+  }
+
+  private void awaitContainerRunning(final DockerClient docker, final String container)
+      throws TimeoutException {
+    Polling.awaitUnchecked(LONG_WAIT_SECONDS, SECONDS, new Callable<Object>() {
+      @Override
+      public Object call() throws Exception {
+        ContainerInfo info = docker.inspectContainer(container);
+        return info != null && info.state().running() ? true : null;
+      }
+    });
+  }
+
+  private String randomName(final String name) {
+    return testTag + "_" + name + "_" + Integer.toHexString(ThreadLocalRandom.current().nextInt());
+  }
 
   @Test
   public void test() throws Exception {
@@ -58,9 +116,15 @@ public class SyslogRedirectionTest extends SystemTestBase {
       final List<String> command = asList("sh", "-c", "echo should-be-redirected");
 
       // Create job
-      final JobId jobId = createJob(testJobName, testJobVersion, BUSYBOX, command,
-                                    ImmutableMap.of("FOO", "4711",
-                                                    "BAR", "deadbeef"));
+      final Job job = Job.newBuilder()
+          .setName(testJobName)
+          .setVersion(testJobVersion)
+          .setImage(BUSYBOX)
+          .setCommand(command)
+          .setVolumesFrom(sink)
+          .build();
+
+      final JobId jobId = createJob(job);
 
       // deploy
       deployJob(jobId, testHost());
